008d81608e3303222f83fcc4a675302170417a93
Elias Reid <47339667+eliasreid@users.noreply.github.com>
2022-04-30 23:53:26 -0300

    initialize simple integer fields with 0

--- BrawlbackPad.h
+++ BrawlbackPad.h
@@ -1,12 +1,12 @@
 #pragma once
 #pragma pack(push, 2)
 struct BrawlbackPad {
-    u16 buttons;
-    u8 stickX;
-    u8 stickY;
-    u8 cStickX;
-    u8 cStickY;
-    u8 LTrigger;
-    u8 RTrigger;
+    u16 buttons = 0;
+    u8 stickX = 0;
+    u8 stickY = 0;
+    u8 cStickX = 0;
+    u8 cStickY = 0;
+    u8 LTrigger = 0;
+    u8 RTrigger = 0;
 };
 #pragma pack(pop)
--- FrameData.h
+++ FrameData.h
@@ -4,7 +4,7 @@
 
 #pragma pack(push, 4)
 struct FrameData {
-    u32 randomSeed;
+    u32 randomSeed = 0;
     PlayerFrameData playerFrameDatas[MAX_NUM_PLAYERS];
 };
 #pragma pack(pop)
