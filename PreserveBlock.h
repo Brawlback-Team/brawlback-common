14543d2121290015d266e41a8dc412b2197a9a6c
Jared White <jwhite2292forward@gmail.com>
2022-05-03 09:26:47 -0500

    Overload the equivalence operator to nullify an error.

--- PreserveBlock.h
+++ PreserveBlock.h
@@ -2,4 +2,8 @@
 struct PreserveBlock {
     u32 address;
     u32 length;
+    bool operator==(const PreserveBlock& p) const
+    {
+      return address == p.address && length == p.length;
+    }
 };
