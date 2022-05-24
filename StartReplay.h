6561a469c706e4debb0340a7872d0d2c7750c7d8
Jared White <jwhite2292forward@gmail.com>
2022-05-23 21:35:14 -0500

    Initialize size of name.

--- StartReplay.h
+++ StartReplay.h
@@ -8,10 +8,10 @@ struct StartReplay
 {
   u32 randomSeed = 0;
   u32 otherRandomSeed = 0;
-  u8 nameSize;
+  u32 firstFrame = 0;
+  u8 nameSize = 0;
   u8 nameBuffer[MAX_REPLAY_NAME_SIZE];
   u8 numPlayers = 0;
-  u8 firstFrame = 0;
   StartReplayPlayer players[MAX_NUM_PLAYERS];
   u8 stage = 0;
 };
