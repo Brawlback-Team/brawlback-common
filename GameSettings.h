2d0224776539e334ab4edda98dc0ada7470a6b5f
Robert Peralta <rapito@gmail.com>
2022-10-07 01:31:18 -0400

    add local port and player types for cpu and none

--- GameSettings.h
+++ GameSettings.h
@@ -2,11 +2,14 @@
 #include "BBConst.h"
 #include "PlayerSettings.h"
 
+#pragma pack(push, 2)
 struct GameSettings
 {
     u8 localPlayerIdx;
+    u8 localPlayerPort;
     u8 numPlayers;
     u16 stageID;
     u32 randomSeed;
     PlayerSettings playerSettings[MAX_NUM_PLAYERS];
 };
+#pragma pack(pop)
--- PlayerType.h
+++ PlayerType.h
@@ -3,4 +3,6 @@ enum PlayerType : u8
 {
     PLAYERTYPE_LOCAL = 0x0,
     PLAYERTYPE_REMOTE = 0x1,
+    PLAYERTYPE_CPU = 0x2,
+    PLAYERTYPE_NONE = 0x3,
 };
