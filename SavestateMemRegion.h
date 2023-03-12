39050b50ab8c82bf37c4a0d9bee0bb2bd27d827d
Jared M. White <jwhite2292forward@gmail.com>
2023-03-12 14:25:59 -0500

    Add buffer for name of the heap

--- SavestateMemRegion.h
+++ SavestateMemRegion.h
@@ -4,6 +4,8 @@ constexpr size_t MAX_HEAPNAME_SIZE = 30;
 
 #pragma pack(push, 4)
 struct SavestateMemRegionInfo {
+    u8 nameBuffer[MAX_HEAPNAME_SIZE];
+    u8 nameSize = 0;
     u32 address;
     u32 size;
     bool TAddFRemove;
