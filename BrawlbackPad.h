aab8d5dd56ce7b4dd3affe654a058365ca8d9ab4
Jared M. White <jwhite2292forward@gmail.com>
2023-04-27 18:40:41 -0500

    Fix pad based on research

--- BrawlbackPad.h
+++ BrawlbackPad.h
@@ -10,12 +10,12 @@ struct BrawlbackPad {
     bu32 rapidFireButtons;
     bu32 releasedButtons;
     bu32 newPressedButtons;
-    bs8 LAnalogue;
-    bs8 RAnalogue;
-    bs8 stickX;
-    bs8 stickY;
-    bs8 cStickX;
-    bs8 cStickY;
+    bu8 LAnalogue;
+    bu8 RAnalogue;
+    bu8 stickX;
+    bu8 stickY;
+    bu8 cStickX;
+    bu8 cStickY;
     
     BrawlbackPad()
     {
@@ -71,12 +71,12 @@ struct BrawlbackPad {
     bu32 rapidFireButtons = 0;
     bu32 releasedButtons = 0;
     bu32 newPressedButtons = 0;
-    bs8 LAnalogue = 0;
-    bs8 RAnalogue = 0;
-    bs8 stickX = 0;
-    bs8 stickY = 0;
-    bs8 cStickX = 0;
-    bs8 cStickY = 0;
+    bu8 LAnalogue = 0;
+    bu8 RAnalogue = 0;
+    bu8 stickX = 0;
+    bu8 stickY = 0;
+    bu8 cStickX = 0;
+    bu8 cStickY = 0;
     #endif
 };
 #pragma pack(pop)
