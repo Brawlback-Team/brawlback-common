0ec0fc31054ac086b541c9bdf24a0405aafbc257
Jared M. White <jwhite2292forward@gmail.com>
2023-09-01 15:58:07 -0500

    Add rumble controls setting

--- BrawlbackControls.h
+++ BrawlbackControls.h
@@ -16,6 +16,7 @@ struct BrawlbackControls {
     bu8 Y;
     bu8 X;
     bu8 tapJumpToggle;
+    bool rumble;
     
     BrawlbackControls();
     BrawlbackControls& operator=(const BrawlbackControls &D );
@@ -33,6 +34,7 @@ struct BrawlbackControls {
     bu8 Y = 0;
     bu8 X = 0;
     bu8 tapJumpToggle = 0;
+    bool rumble = false;
     #endif
 };
 #pragma pack(pop)
