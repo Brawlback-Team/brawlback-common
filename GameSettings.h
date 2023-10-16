c9cb2143471da0587e95182aba3f7299dbbcb059
Jared M. White <jwhite2292forward@gmail.com>
2023-10-16 13:10:27 -0500

    Add default values for GameSettings and PlayerSettings

--- GameSettings.h
+++ GameSettings.h
@@ -16,11 +16,11 @@ struct GameSettings
     GameSettings(const GameSettings &D);
     GameSettings& operator = (const GameSettings &D );
     #else
-    bu8 localPlayerIdx;
-    bu8 localPlayerPort;
-    bu8 numPlayers;
-    bu16 stageID;
-    bu32 randomSeed;
+    bu8 localPlayerIdx = 0;
+    bu8 localPlayerPort = 0;
+    bu8 numPlayers = 0;
+    bu16 stageID = 0;
+    bu32 randomSeed = 0;
     PlayerSettings playerSettings[MAX_NUM_PLAYERS];
     #endif
 };
--- PlayerSettings.h
+++ PlayerSettings.h
@@ -22,12 +22,12 @@ struct PlayerSettings
     PlayerSettings(const PlayerSettings &D);
     PlayerSettings& operator=(const PlayerSettings &D );
     #else
-    bu8 charID;
-    bu8 charColor;
-    bu8 colorFileIndex;
-    PlayerType playerType;
-    bu8 controllerPort;
-    bool rumble;
+    bu8 charID = 0;
+    bu8 charColor = 0;
+    bu8 colorFileIndex = 0;
+    PlayerType playerType = 3;
+    bu8 controllerPort = 0;
+    bool rumble = false;
     bu16 nametag[NAMETAG_SIZE];
     bu8 displayName[DISPLAY_NAME_SIZE];
     bu8 connectCode[CONNECT_CODE_SIZE];
