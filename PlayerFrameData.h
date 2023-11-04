6be80aa3aeb1de7e63c081704d9660d4dc2a2065
Jared M. White <jwhite2292forward@gmail.com>
2023-11-04 08:20:02 -0500

    Move array to PlayerFrameData level

--- PlayerFrameData.h
+++ PlayerFrameData.h
@@ -3,14 +3,13 @@
 #include "SyncData.h"
 #include "BrawlbackTypes.h"
 #include "BrawlbackControls.h"
-#include "BrawlbackConstants.h"
 #pragma pack(push, 4)
 struct PlayerFrameData {
     #if __cplusplus == 199711L
     bu32 frame;
-    BrawlbackPad pad[MAX_ROLLBACK_FRAMES];
-    BrawlbackPad sysPad[MAX_ROLLBACK_FRAMES];
-    SyncData syncData[MAX_ROLLBACK_FRAMES];
+    BrawlbackPad pad;
+    BrawlbackPad sysPad;
+    SyncData syncData;
     bu8 playerIdx;
 
     PlayerFrameData();
@@ -18,9 +17,9 @@ struct PlayerFrameData {
     PlayerFrameData& operator=(const PlayerFrameData &D );
     #else
     bu32 frame = 0;
-    BrawlbackPad pad[MAX_ROLLBACK_FRAMES];
-    BrawlbackPad sysPad[MAX_ROLLBACK_FRAMES];
-    SyncData syncData[MAX_ROLLBACK_FRAMES];
+    BrawlbackPad pad;
+    BrawlbackPad sysPad;
+    SyncData syncData;
     bu8 playerIdx = 0;
     #endif
 };
