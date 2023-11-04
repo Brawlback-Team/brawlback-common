d151cd50bbc8ddf3b2a4be55fe732b1ae9327fa7
Jared M. White <jwhite2292forward@gmail.com>
2023-11-03 19:12:18 -0500

    Adjust PlayerFrameData for input processing rework

--- PlayerFrameData.h
+++ PlayerFrameData.h
@@ -3,25 +3,24 @@
 #include "SyncData.h"
 #include "BrawlbackTypes.h"
 #include "BrawlbackControls.h"
+#include "BrawlbackConstants.h"
 #pragma pack(push, 4)
 struct PlayerFrameData {
     #if __cplusplus == 199711L
-    bu32 randomSeed;
     bu32 frame;
-    BrawlbackPad pad;
-    BrawlbackPad sysPad;
-    SyncData syncData;
+    BrawlbackPad pad[MAX_ROLLBACK_FRAMES];
+    BrawlbackPad sysPad[MAX_ROLLBACK_FRAMES];
+    SyncData syncData[MAX_ROLLBACK_FRAMES];
     bu8 playerIdx;
 
     PlayerFrameData();
     PlayerFrameData(const PlayerFrameData &D);
     PlayerFrameData& operator=(const PlayerFrameData &D );
     #else
-    bu32 randomSeed = 0;
     bu32 frame = 0;
-    BrawlbackPad pad;
-    BrawlbackPad sysPad;
-    SyncData syncData;
+    BrawlbackPad pad[MAX_ROLLBACK_FRAMES];
+    BrawlbackPad sysPad[MAX_ROLLBACK_FRAMES];
+    SyncData syncData[MAX_ROLLBACK_FRAMES];
     bu8 playerIdx = 0;
     #endif
 };
