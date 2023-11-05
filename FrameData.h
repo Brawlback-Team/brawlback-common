f09ae5d5bc222f5b8da493ec8a3fae8a4e9243b4
Jared M. White <jwhite2292forward@gmail.com>
2023-11-05 07:55:10 -0600

    Fix numInputs and compile issues

--- FrameData.h
+++ FrameData.h
@@ -6,17 +6,17 @@
 struct FrameData {
     #if __cplusplus == 199711L
     bu32 randomSeed;
+    bs32 numInputs[MAX_NUM_PLAYERS];
     PlayerFrameData playerFrameDatas[MAX_ROLLBACK_FRAMES][MAX_NUM_PLAYERS];
     bu8 frameResult;
-    bu8 numInputs;
     FrameData();
     FrameData(const FrameData& D);
     FrameData& operator=(const FrameData &D );
     #else
     bu32 randomSeed = 0;
+    bs32 numInputs[MAX_NUM_PLAYERS];
     PlayerFrameData playerFrameDatas[MAX_NUM_PLAYERS][MAX_ROLLBACK_FRAMES];
     bu8 frameResult = 1;
-    bu8 numInputs = 0;
     #endif
 };
 #pragma pack(pop)
